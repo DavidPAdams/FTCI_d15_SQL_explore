c752cc267d03e8306f1eeeafd0fd428d5ec7e142
David Adams <david.riding.rails@gmail.com>
2020-03-24 18:51:15 -0700

    initial commit and application.properties

---
+++ src/main/java/com/tts/d15_dbExplore/D15DbExploreApplication.java
@@ -0,0 +1,13 @@
+package com.tts.d15_dbExplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D15DbExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D15DbExploreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/d15_dbExplore/D15DbExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.d15_dbExplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D15DbExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
